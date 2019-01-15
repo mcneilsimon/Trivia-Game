d25c1fd7886612f61845f9256b63bd814e47b12b
Simon Mc Neil <mcneils@sheridancollege.ca>
2019-01-15 00:35:52 -0500

    Initial Commit

---
+++ Trivia Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia Game
+//
+//  Created by Simon Mc Neil on 2019-01-15.
+//  Copyright © 2019 Simon Mc Neil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
